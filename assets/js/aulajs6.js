37646bbb4e3e2a1cc73e061c80fd70c62c29b8b0
GuiLimaSeila <guilhermehenriquelima@gmail.com>
2023-05-02 10:03:45 -0300

    first commit

--- assets/js/aulajs6.js
+++ assets/js/aulajs6.js
@@ -1,8 +1,24 @@
 let nome = prompt("bota um numero")
 let i = [];
+let soma = 0;
+let div = 0;
+let a = 0
+let maiorNumero = 0;
+let menorNumero = i[a];
 
-while (nome != "pare") {
+while (nome != "a") {
+    a++
     i.push(nome);
     nome = prompt("bota um numero")
+    div++
+    var rato = i[a]
+    soma += rato
+    if (rato > maiorNumero) {
+        maiorNumero = rato;
+    }
+    if (rato < menorNumero) {
+        menorNumero = rato;
+    }
 }
-alert("Seus numeros foram: " + i)
+
+alert("Seus numeros foram: " + i + "\n" + `A soma é ${soma}` + "\n" + `Sua divição é ${soma / div}` + "\n" + `Maior numero: ${maiorNumero}` + "\n" + `Menor numero: ${menorNumero}`)
