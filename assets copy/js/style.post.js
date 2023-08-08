bf9cbd451d0d0142588ee33fbf7b685fcc72668c
GuiLimaSeila <guilhermehenriquelima@gmail.com>
2023-08-08 14:31:55 -0300

    feat: create store post function

--- assets copy/js/style.post.js
+++ assets copy/js/style.post.js
@@ -1,20 +1,36 @@
+const posts = []
 
-function savePost(){
-const title= document.getElementById("title").value;
-const category= document.getElementById("category").value;
-const resume= document.getElementById("resume").value;
-const author= document.getElementById("author").value;
-const date= document.getElementById("date").value;
+function savePost() {
+    const title = document.getElementById("title").value;
+    const category = document.getElementById("category").value;
+    const resume = document.getElementById("resume").value;
+    const author = document.getElementById("author").value;
+    const date = document.getElementById("date").value;
 
-console.log(title, category, resume, author, date)
-
-cleanFields()
+    if (title && category && resume && author && date) {
+        storePost(title, category, resume, author, date);
+        cleanFields();
+    } else{
+        alert("Preencha todos os campos!!!");
+    }
 }
 
-function cleanFields(){
+function cleanFields() {
     document.getElementById("title").value = "";
     document.getElementById("category").value = "";
     document.getElementById("resume").value = "";
     document.getElementById("author").value = "";
     document.getElementById("date").value = "";
+}
+
+function storePost(title, category, resume, author, date) {
+    const post = {
+        title,
+        category,
+        resume,
+        author,
+        date,
+    };
+    posts.push(post);
+    console.log(posts);
 }
