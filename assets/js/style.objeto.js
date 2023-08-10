cee313e60caae59e7bee7893525d74746d8f0be3
GuiLimaSeila <guilhermehenriquelima@gmail.com>
2023-08-10 08:11:02 -0300

    feat: create new object in javascript

---
+++ assets/js/style.objeto.js
@@ -0,0 +1,13 @@
+let personSimple = "John Doe";
+
+const personObject = {
+firstName : "John",
+lastName : "Doe",
+age : 50,
+eyeColor : "blue"
+}
+
+personObject.age = 10; //Mudando para 10
+
+console.log(personSimple);
+console.log(personObject);
