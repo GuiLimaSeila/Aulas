cf84b6c824aa5f22b04bbaddafee77227fe3fae7
GuiLimaSeila <guilhermehenriquelima@gmail.com>
2023-08-15 15:05:27 -0300

    feat: doing js

--- assets/js/tasklist.js
+++ assets/js/tasklist.js
@@ -1,41 +1,64 @@
 let takIndex = -1
 
-class Task{
-    constructor(id, title, status){
+class Task {
+    constructor(id, title, status) {
         this.id = id;
         this.title = title;
         this.status = status;
     }
 }
 
-class TaskList{
-
+class TaskList {
+    constructor(){
+this.tasks =[]
 }
-function randomID(){
-let id = Math.floor(Math.random() * 9999)
+addPost(tasks)
+}
+
+
+
+function randomID() {
+    let id = Math.floor(Math.random() * 9999)
 }
 
 const taskList = new TaskList()
 
-function creatTask(){
+function creatTask() {
     let title = document.getElementById("resume").value;
-
-    const nuevo =new Task(randomID(), title, false)
-        if (title) {
-            if (takIndex == -1) {
-                storePost();
-                cleanFields();
-                showPosts();
-            } else {
-                Task[takIndex] = {
-                    title,
-                };
-            }
-            cleanFields();
-            showPosts();
-            postsIndex = -1
-        } else {
-            document.getElementById("addSomething").classList.remove("hidden")
-        }
+    const nuevo = new Task(randomID(), title, false)
+    taskList.addPost(nuevo)
 
 }
+function cleanFields() {
+    document.getElementById("resume").value = "";
+}
+
+function showPosts() {
+    creatTask()
+    let showContent = "";
+
+    taskList.tasks.forEach((nuevo, index) => {
+        showContent += `
+        <div class="itemPost">
+        <div class="textItem">
+            <p class="textPost">${nuevo.title}</p>
+        </div>
+        <div class="buttonArea">
+            <button onclick="verifyPost(${index})" class="checkButton"><i class="fa-solid fa-check"></i></button>
+            <button onclick="editPost(${index})" class="editButton"><i class="fa-solid fa-pen"></i></button>
+            <button onclick="removePost(${index})" class="deletButton"><i class="fa-solid fa-trash"></i></button>
+        </div>
+    </div>
+            `;
+    });
+
+    document.getElementById("postForm").innerHTML = showContent;
+    title = document.getElementById("resume").value= "";
+}
+function removePost(index){
+    taskList.tasks.splice(index, 1);
+    showPosts();
+
+}
+
+function verifyPost()
