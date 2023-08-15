cdf1ea483e0e301828f99555cfe4d9dd4bee998b
GuiLimaSeila <guilhermehenriquelima@gmail.com>
2023-08-15 14:32:50 -0300

    feat: finished css and starting js

--- assets/js/tasklist.js
+++ assets/js/tasklist.js
@@ -1,22 +1,41 @@
-const posts = []
-let postsIndex = -1
+let takIndex = -1
 
-function savePost() {
-    const resume = document.getElementById("resume").value;
-    if (resume) {
-        if (postsIndex == -1) {
-            storePost(resume);
+class Task{
+    constructor(id, title, status){
+        this.id = id;
+        this.title = title;
+        this.status = status;
+    }
+}
+
+class TaskList{
+
+}
+function randomID(){
+let id = Math.floor(Math.random() * 9999)
+}
+
+const taskList = new TaskList()
+
+function creatTask(){
+    let title = document.getElementById("resume").value;
+
+    const nuevo =new Task(randomID(), title, false)
+        if (title) {
+            if (takIndex == -1) {
+                storePost();
+                cleanFields();
+                showPosts();
+            } else {
+                Task[takIndex] = {
+                    title,
+                };
+            }
             cleanFields();
             showPosts();
+            postsIndex = -1
         } else {
-            posts[postsIndex] = {
-                resume,
-            };
+            document.getElementById("addSomething").classList.remove("hidden")
         }
-        cleanFields();
-        showPosts();
-        postsIndex = -1
-    } else {
-        document.getElementById("addSomething").classList.remove("hidden")
-    }
+
 }
