37f6c96cfcd04060d20c09cff890cd1e149d5cb4
GuiLimaSeila <guilhermehenriquelima@gmail.com>
2023-08-15 16:25:42 -0300

    feat: doing js

--- assets/js/tasklist.js
+++ assets/js/tasklist.js
@@ -9,19 +9,22 @@ class Task {
 }
 
 class TaskList {
-    constructor(){
-this.tasks =[]
-}
-addPost(task){
-    this.tasks.push(task)
-}
-deletePost(id){
-    this.tasks = this.tasks.filter(task => task.id != id)
-}
-editPost(){
-    const post = taskList.tasks[index]
-    document.getElementById("resume")
-}
+    constructor() {
+        this.tasks = []
+    }
+    addPost(task) {
+        this.tasks.push(task)
+    }
+    deletePost(id) {
+        this.tasks = this.tasks.filter(task => task.id != id)
+    }
+    editPost() {
+        const post = taskList.tasks[index]
+        document.getElementById("resume") = post.title
+    }
+    verified(id) {
+        this.tasks.forEach
+    }
 }
 
 
@@ -36,14 +39,27 @@ function creatTask() {
     let title = document.getElementById("resume").value;
     const nuevo = new Task(randomID(), title, false)
     taskList.addPost(nuevo)
-
+    if (title) {
+        if (takIndex == -1) {
+            cleanFields();
+            showPosts();
+        } else {
+            taskList.tasks[takIndex] = {
+                title,
+            };
+        }
+        cleanFields();
+        showPosts();
+        takIndex = -1
+    } else {
+        document.getElementById("addSomething").classList.remove("hidden")
+    }
 }
 function cleanFields() {
     document.getElementById("resume").value = "";
 }
 
 function showPosts() {
-    creatTask()
     let showContent = "";
 
     taskList.tasks.forEach((nuevo, index) => {
@@ -53,23 +69,19 @@ function showPosts() {
             <p class="textPost">${nuevo.title}</p>
         </div>
         <div class="buttonArea">
-            <button onclick="verifyPost(${index})" class="checkButton"><i class="fa-solid fa-check"></i></button>
+            <button onclick="verifyPost(${index.status})" class="checkButton"><i class="fa-solid fa-check"></i></button>
             <button onclick="editPost(${index})" class="editButton"><i class="fa-solid fa-pen"></i></button>
-            <button onclick="removePost(${index})" class="deletButton"><i class="fa-solid fa-trash"></i></button>
+            <button onclick="removePost(${index.id})" class="deletButton"><i class="fa-solid fa-trash"></i></button>
         </div>
     </div>
             `;
     });
 
     document.getElementById("postArea").innerHTML = showContent;
-    title = document.getElementById("resume").value= "";
+    title = document.getElementById("resume").value = "";
 }
-function removePost(index){
+function removePost(index) {
     taskList.tasks.splice(index, 1);
     showPosts();
-
-}
-
-function verifyPost(index){
 
 }
