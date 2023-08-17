c37fbafde025fcb73044fbc09b72cf53ec81c6cb
GuiLimaSeila <guilhermehenriquelima@gmail.com>
2023-08-17 14:28:11 -0300

    feat: initiciate html

--- assets/js/categoryproduct.js
+++ assets/js/categoryproduct.js
@@ -38,20 +38,26 @@ class ProductService{
         const id= this.nextproductID ++;
         const product = new Product(id, name, price, category);
         category.products.push(product);
-        this.products.push(product)
+        this.products.push(product);
     }
 }
 
 const categoryList = new CategoryService()
 const productList = new ProductService()
 
+
 function createCategory(){
     const categoryName = "Doce";
-    const productName = "Kit-Kat";
-
     categoryList.addCategory(categoryName);
-    productList.addProduct(productName);
+    console.log(categoryList.categories);
+    createProduct()
+}
 
-    console.log(categoryList.categories)
+function createProduct(){
+    const productName = "Bolo";
+    const productPrice = 20;
+    const productCategory = categoryList.categories[0];
+
+    productList.addProduct(productName, productPrice, productCategory)
     console.log(productList.products)
 }
