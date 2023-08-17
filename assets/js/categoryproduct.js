a2949881183ff8e65f6701dbb10443be842ec26d
GuiLimaSeila <guilhermehenriquelima@gmail.com>
2023-08-17 17:08:27 -0300

    feat: display function

--- assets/js/categoryproduct.js
+++ assets/js/categoryproduct.js
@@ -21,21 +21,21 @@ class CategoryService {
         this.nextcategoryID = 0;
     }
 
-    addCategory(name){
-        const id = this.nextcategoryID ++;
+    addCategory(name) {
+        const id = this.nextcategoryID++;
         const category = new Category(id, name);
         this.categories.push(category);
     }
 }
 
-class ProductService{
-    constructor(){
+class ProductService {
+    constructor() {
         this.products = [];
-        this.nextproductID = 0; 
+        this.nextproductID = 0;
     }
 
-    addProduct(name, price, category){
-        const id= this.nextproductID ++;
+    addProduct(name, price, category) {
+        const id = this.nextproductID++;
         const product = new Product(id, name, price, category);
         category.products.push(product);
         this.products.push(product);
@@ -46,20 +46,22 @@ const categoryList = new CategoryService()
 const productList = new ProductService()
 
 
-function createCategory(){
+function createCategory() {
     const categoryName = document.getElementById("categoryName").value;
     categoryList.addCategory(categoryName);
     console.log(categoryList.categories);
-    cleanFormFields() 
+    cleanFormFields()
 }
 
-function createProduct(){
+function createProduct() {
     const productName = "Bolo";
     const productPrice = 20;
     const productCategory = categoryList.categories[0];
 
     productList.addProduct(productName, productPrice, productCategory)
     console.log(productList.products)
+    displayCategoriesAndProducts()
+    cleanFormFields()
 }
 
 function cleanFormFields() {
@@ -67,4 +69,39 @@ function cleanFormFields() {
     document.getElementById("productName").value = "";
     document.getElementById("productPrice").value = "";
     document.getElementById("productCategory").value = "";
+}
+
+function displayCategoriesAndProducts() {
+    let content = "";
+
+    categoryList.categories.forEach((category) => {
+        content += `
+        <li>
+<div class="categoriesList">
+<span><b>Categoria:</b> ${category.name}</span>
+<div>
+<button class="editeButton"><i class="fa-solid fa-pen"></i></button>
+<button class="deleteButton"><i class="fa-solid fa-trash"></i></button>
+</div>
+</div>
+        </li>
+        <ul class="productsListByCategory">`;
+        category.products.forEach((product) => {
+            content += `
+            <li>
+            <div class="productsList">
+            <span><b>Produto:</b> ${product.name} - <b>Preço:</b> ${product.price}</span>
+<div>
+<button class="editeButton"><i class="fa-solid fa-pen"></i></button>
+<button class="deleteButton"><i class="fa-solid fa-trash"></i></button>
+</div>
+</div>`
+
+        
+    });
+    content += `
+    </ul>
+    </li>`;
+});
+    document.getElementById("categoriesList").innerHTML = content;
 }
