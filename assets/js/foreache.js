77636f82471a56e7c4d60a1665b5f7d0310c97ef
GuiLimaSeila <guilhermehenriquelima@gmail.com>
2023-08-17 08:31:19 -0300

    feat: activits

---
+++ assets/js/foreache.js
@@ -0,0 +1,41 @@
+console.error("empresas");
+const companies = ["Apple", "Google", "Facebook"];
+
+companies.forEach(company => {
+    console.log(`Hey, ${company}`);
+});
+
+console.error("numeros pares");
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+numbers.forEach((number, index) => {
+    if (number % 2 == 0){
+        console.log("Isso é um numero par: " + number);
+        console.log("Esse é seu Index: " + index)
+    }
+});
+
+console.error("marcas de carros e seus modelos");
+const cars = [
+    {
+        marca: "Ford",
+        modelo: "Focus",
+    },
+    {
+        marca: "BMW",
+        modelo: "BMW Z4",
+    },
+    {
+        marca: "Fiat",
+        modelo: "Palio",
+    },
+    {
+        marca: "Audi",
+        modelo: "a3",
+    },
+];
+
+cars.forEach((car) => {
+    console.log(`Marca: ${car.marca} -
+    Modelo: ${car.modelo}`);
+});
