fa1e4be42169b1e2cd0db51531202e2d556fd203
GuiLimaSeila <guilhermehenriquelima@gmail.com>
2023-08-17 10:32:55 -0300

    feat: create addCategoryService method

--- assets/js/categoryproduct.js
+++ assets/js/categoryproduct.js
@@ -1,5 +1,5 @@
-class Category{
-    constructor(id, name){
+class Category {
+    constructor(id, name) {
         this.id = id;
         this.name = name;
         this.products = [];
@@ -7,9 +7,23 @@ class Category{
 }
 
 class Product {
-    constructor(id, name, price){
+    constructor(id, name, price, category) {
         this.id = id;
         this.name = name;
         this.price = price;
+        this.category = category;
+    }
+}
+
+class CategoryService {
+    constructor() {
+        this.categories = [];
+        this.nextcategoryID = 0;
+    }
+
+    addCategory(name){
+        const id = this.nextcategoryID ++;
+        const category = new Category(id, name);
+        this.categories.push(category)
     }
 }
